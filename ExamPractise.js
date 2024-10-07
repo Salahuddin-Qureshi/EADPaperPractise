6ab6f20722a2c51fe7634249bfecb52b180d4af5
Salahuddin Qureshi <salahuddinq.cs@gmail.com>
2024-10-08 02:01:00 +0500

    Initial COmmit to ExamPractise.js

---
+++ ExamPractise.js
@@ -0,0 +1,63 @@
+// console.log('A')
+// function fetchData(callback){
+//     setTimeout(()=>{
+//         callback('Data From Server');
+//     }, 2000)
+
+//     {
+//         console.log('Hello')
+//     } ;
+// }
+
+// fetchData((data) =>{
+//     console.log(data)
+// })
+// console.log('B')
+
+
+// function PromiseReturn() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve('Data From Server')
+//             // reject('Error')
+//         }, 3000);
+//     })
+// }
+// PromiseReturn().then(data => console.log(data))
+// .then(() => { console.log('This is actual data from server') })
+// .catch((error) => {
+//     console.log(error)
+// })
+
+
+original = {
+    name: 'John',
+    age: 30,
+    address: {
+        street: '123 Main St',
+        city: 'New York',
+        zip: '4362-A',
+        country: 'USA'   
+    }
+}
+
+console.log(original)
+const shallowCopy = {...original}
+console.log(shallowCopy)
+
+shallowCopy.name = 'Salahuddin';
+// shallowCopy.address.street = '456 Main St';
+console.log(shallowCopy)
+
+console.log(original.address.street === shallowCopy.address.street)
+console.log(original.name === shallowCopy.name)
+
+const deepCopy = JSON.parse(JSON.stringify(original));
+
+console.log(deepCopy)
+deepCopy.name = 'Alex'
+deepCopy.address.street = 'Ayaz Gul Street'
+console.log(original.address.street === deepCopy.address.street)
+console.log(original.name === deepCopy.name)
+
+console.log('Hello')
